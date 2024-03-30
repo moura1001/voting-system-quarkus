4b552199c6a3c1fe4d8077d9d2265d57f27b9457
moura1001 <gmouraneto01@gmail.com>
2024-03-30 19:50:44 -0300

    test: adição de casos de teste para garantir a correta listagem e busca por nome de um determinado candidato no repository

--- election-management/src/test/java/domain/CandidateStorageTest.java
+++ election-management/src/test/java/domain/CandidateStorageTest.java
@@ -1,8 +1,10 @@
 package domain;
 
 import org.instancio.Instancio;
+import org.instancio.Select;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +21,30 @@ public abstract class CandidateStorageTest {
 
         assertTrue(result.isPresent());
         assertEquals(candidate, result.get());
+    }
+
+    @Test
+    void shouldBeFindAllCandidates() {
+        List<Candidate> candidates = Instancio.stream(Candidate.class).limit(10).toList();
+        storage().save(candidates);
+
+        List<Candidate> result = storage().getAllCandidates();
+
+        assertTrue(result.size() == candidates.size() && result.containsAll(candidates) && candidates.containsAll(result));
+    }
+
+    @Test
+    void shouldBeFindCandidateByName() {
+        Candidate candidate1 = Instancio.create(Candidate.class);
+        Candidate candidate2 = Instancio.of(Candidate.class)
+                .set(Select.field("familyName"), "Moura").create();
+
+        CandidateQuery query = new CandidateQuery.Builder().name("Moura").build();
+
+        storage().save(List.of(candidate1, candidate2));
+
+        List<Candidate> result = storage().getAllCandidates(query);
+        assertEquals(1, result.size());
+        assertEquals(candidate2, result.get(0));
     }
 }
