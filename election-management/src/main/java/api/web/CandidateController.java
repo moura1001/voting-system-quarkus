698c4d62dab91d578ad86b361a603afbd328c13e
moura1001 <gmouraneto01@gmail.com>
2024-03-31 13:22:35 -0300

    test + feat: finalização dos testes unitários com a api rest adicionando casos de teste para atualização de dados e listagem dos candidatos para garantir a corretude dos códigos de resposta retornados

--- election-management/src/main/java/api/web/CandidateController.java
+++ election-management/src/main/java/api/web/CandidateController.java
@@ -2,16 +2,16 @@ package api.web;
 
 import api.CandidateApi;
 import api.dto.request.CandidateCreateDTO;
+import api.dto.request.CandidateUpdateDTO;
+import api.dto.response.CandidateResponseDTO;
 import org.jboss.resteasy.reactive.ResponseStatus;
 import org.jboss.resteasy.reactive.RestResponse;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,5 +25,21 @@ public class CandidateController {
     @Transactional
     public void create(CandidateCreateDTO candidateDTO) {
         candidateApi.create(candidateDTO);
+    }
+
+    @GET
+    public List<CandidateResponseDTO> list() {
+        return candidateApi.list();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @ResponseStatus(RestResponse.StatusCode.ACCEPTED)
+    @Transactional
+    public CandidateResponseDTO update(
+            @PathParam("id") String id,
+            CandidateUpdateDTO candidateDTO
+    ) {
+        return candidateApi.update(id, candidateDTO);
     }
 }
--- election-management/src/test/java/api/web/CandidateControllerTest.java
+++ election-management/src/test/java/api/web/CandidateControllerTest.java
@@ -2,6 +2,8 @@ package api.web;
 
 import api.CandidateApi;
 import api.dto.request.CandidateCreateDTO;
+import api.dto.request.CandidateUpdateDTO;
+import api.dto.response.CandidateResponseDTO;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
@@ -11,10 +13,14 @@ import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.MediaType;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 @QuarkusTest
 @TestHTTPEndpoint(CandidateController.class)
@@ -31,6 +37,40 @@ class CandidateControllerTest {
                         .then().statusCode(RestResponse.StatusCode.CREATED);
 
         verify(candidateApi).create(candidateDTO);
+        verifyNoMoreInteractions(candidateApi);
+    }
+
+    @Test
+    void shouldBeListAllCandidates() {
+        List<CandidateResponseDTO> candidates = Instancio.stream(CandidateResponseDTO.class).limit(4).collect(Collectors.toList());
+
+        when(candidateApi.list()).thenReturn(candidates);
+
+        var response = given()
+                .when().get()
+                .then().statusCode(RestResponse.StatusCode.OK).extract().as(CandidateResponseDTO[].class);
+
+        verify(candidateApi).list();
+        verifyNoMoreInteractions(candidateApi);
+
+        List<CandidateResponseDTO> result = Arrays.stream(response).toList();
+
+        assertTrue(candidates.size() == result.size() && result.containsAll(candidates) && candidates.containsAll(result));
+    }
+
+    @Test
+    void shouldBeUpdateInfoAboutACandidate() {
+        String id = UUID.randomUUID().toString();
+        CandidateUpdateDTO updateDTO = Instancio.create(CandidateUpdateDTO.class);
+        CandidateResponseDTO result = CandidateResponseDTO.toDTO(updateDTO.toDomain(id));
+
+        when(candidateApi.update(id, updateDTO)).thenReturn(result);
+
+        given().contentType(MediaType.APPLICATION_JSON).body(updateDTO)
+                .when().put("/"+id)
+                .then().statusCode(RestResponse.StatusCode.ACCEPTED);
+
+        verify(candidateApi).update(id, updateDTO);
         verifyNoMoreInteractions(candidateApi);
     }
 }
