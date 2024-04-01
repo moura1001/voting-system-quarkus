831dc37f070daf804e32655a0ae3b3c9151277df
moura1001 <gmouraneto01@gmail.com>
2024-04-01 11:31:16 -0300

    feat: election-management -> utilização do redis como um message broker para lidar com o cenário onde várias aplicações devem ser notificadas quando uma eleição é iniciada

--- election-management/src/main/java/infra/repository/ElectionRedisStorage.java
+++ election-management/src/main/java/infra/repository/ElectionRedisStorage.java
@@ -3,6 +3,7 @@ package infra.repository;
 import domain.Election;
 import domain.ElectionStorage;
 import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.pubsub.PubSubCommands;
 import io.quarkus.redis.datasource.sortedset.SortedSetCommands;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -15,9 +16,11 @@ import java.util.stream.Collectors;
 public class ElectionRedisStorage implements ElectionStorage {
 
     private final SortedSetCommands<String, String> commands;
+    private final PubSubCommands<String> pubSubCommands;
 
     public ElectionRedisStorage(RedisDataSource redisDataSource) {
         commands = redisDataSource.sortedSet(String.class, String.class);
+        pubSubCommands = redisDataSource.pubsub(String.class);
     }
 
     @Override
@@ -29,5 +32,6 @@ public class ElectionRedisStorage implements ElectionStorage {
                 ));
 
         commands.zadd("election-"+election.id(), rank);
+        pubSubCommands.publish("elections", election.id());
     }
 }
