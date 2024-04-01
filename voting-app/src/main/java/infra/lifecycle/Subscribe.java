1af869e54d9a6f45a4541a62e727315f04f3b663
moura1001 <gmouraneto01@gmail.com>
2024-04-01 16:43:07 -0300

    fix: voting-app -> alteração no subscribe do canal para lidar com o paradigma reativo do quarkus e sua arquitetura de Event Loop assíncrona e não bloqueante, assim como o Node.js

--- voting-app/src/main/java/infra/lifecycle/Subscribe.java
+++ voting-app/src/main/java/infra/lifecycle/Subscribe.java
@@ -2,8 +2,10 @@ package infra.lifecycle;
 
 import domain.Election;
 import infra.repository.ElectionRedisRepository;
-import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.ReactiveRedisDataSource;
 import io.quarkus.runtime.Startup;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,15 +16,23 @@ public class Subscribe {
     private static final Logger LOGGER = Logger.getLogger(Subscribe.class);
 
     public Subscribe(
-            RedisDataSource redisDataSource,
+            ReactiveRedisDataSource reactiveRedisDataSource,
             ElectionRedisRepository repository
     ) {
         LOGGER.info("Startup: Subscribe");
 
-        redisDataSource.pubsub(String.class).subscribe("elections", id -> {
+        Multi<String> sub = reactiveRedisDataSource.pubsub(String.class).subscribe("elections");
+        sub.emitOn(Infrastructure.getDefaultWorkerPool())
+                .subscribe().with(id -> {
+                    LOGGER.info("Election " + id + "received from subscription");
+                    Election election = repository.getElectionById(id);
+                    LOGGER.info("Election " + election.id() + " starting...");
+                });
+
+        /*redisDataSource.pubsub(String.class).subscribe("elections", id -> {
             LOGGER.info("Election " + id + "received from subscription");
             Election election = repository.getElectionById(id);
             LOGGER.info("Election " + election.id() + " starting...");
-        });
+        });*/
     }
 }
