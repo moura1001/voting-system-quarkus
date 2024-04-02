6c74b2f8f40195f1a4ef45415b61d44a07f3fec7
moura1001 <gmouraneto01@gmail.com>
2024-04-02 17:21:13 -0300

    feat: client-app-react -> finalização da interface de usuário para exibição do resultado parcial da votação em near real time

--- client-app-react/src/App.js
+++ client-app-react/src/App.js
@@ -1,23 +1,58 @@
+import React from "react";
+import _ from 'lodash';
+
 import logo from './logo.svg';
 import './App.css';
 
+function Election() {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    let eventSource = new EventSource("http://vote.dio.localhost");
+    eventSource.onmessage = e => {
+      updateElection(JSON.parse(e.data));
+    }
+  }, []);
+
+  const updateElection = (election) => {
+    setData([...election])
+  }
+
+  return (
+    <div>
+      {data.map((election) =>
+        <div key={election.id}>
+          <h1>Election {election.id}</h1>
+
+          <div className="election">
+            {_.orderBy(election.candidates, ['votes', 'fullName'], ['desc', 'asc']).map((candidate) =>
+            <div key={candidate.id} className="card" data-id={candidate.id}>
+              <div className="votes">{candidate.votes}</div>
+              <div className="profile">
+                <img src={candidate.photo} alt={candidate.fullName} className="thumbnail" width="180" height="180"/>
+              </div>
+              <h3 className="name">{candidate.fullName}</h3>
+              <p className="title">{candidate.jobTitle}</p>
+              <a className="description" href={"mailto:" + candidate.email} target="_blank" rel="noreferrer">{candidate.email}</a>
+              { candidate.phone ? <a className="description" href={"tel:" + candidate.phone} target="_blank" rel="noreferrer">{candidate.phone}</a> : ''}
+            </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
       </header>
+      <main>
+        <Election />
+      </main>
     </div>
   );
 }
